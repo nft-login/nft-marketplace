963bc8a238ff4733dc4b725e266fef5c5836ad74
Christian M <chriamue@gmail.com>
2021-11-19 10:43:45 +0100

    :sparkles: added spinner

---
+++ src/components/Loading.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import RotateLoader from "vue-spinner/src/RotateLoader.vue";
+const loading = true;
+const color = "#4289b9";
+</script>
+
+<template>
+  <rotate-loader :loading="loading" :color="color" />
+</template>
+
+<style scoped>
+</style>
