2a82139ef89aee512c8cc14ab88631661e3e3f5e
Christian M <chriamue@gmail.com>
2021-11-21 19:39:10 +0100

    :sparkles: upload description using web3.storage

--- src/components/Deploy.vue
+++ src/components/Deploy.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
+const WEB3_STORAGE_API_TOKEN = import.meta.env.VITE_WEB3_STORAGE_API_TOKEN;
+
 import { ref } from "vue";
 import Loading from "./Loading.vue";
 import { useStore } from "../store";
+import { Web3Storage } from "web3.storage";
 
-const ipfs = window.IpfsHttpClient.create({
-  host: "ipfs.infura.io",
-  port: 5001,
-  protocol: "https",
-});
+const uploadDescription = async (
+  name: string,
+  description: string
+): Promise<string> => {
+  if (WEB3_STORAGE_API_TOKEN && typeof WEB3_STORAGE_API_TOKEN === "string") {
+    const web3Storage = new Web3Storage({ token: WEB3_STORAGE_API_TOKEN });
+    const file = new File([description], `${name}.md`, { type: "text/plain" });
+    const rootCid = await web3Storage.put([file]);
+    const res: any = await web3Storage.get(rootCid);
+    const files = await res.files();
+    console.log(WEB3_STORAGE_API_TOKEN, files[0].cid);
+    return `https://ipfs.infura.io/ipfs/${files[0].cid}/?token=`;
+  } else {
+    const ipfs = window.IpfsHttpClient.create({
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https",
+    });
+
+    const cid = await ipfs.add(description);
+    return `https://ipfs.infura.io/ipfs/${cid.path}/?token=`;
+  }
+};
+
 const store = useStore();
 
 const name = ref("My Super NFT");
@@ -17,8 +39,7 @@ const loading = ref(false);
 const deploy = async (e: any) => {
   e.preventDefault();
   loading.value = true;
-  const cid = await ipfs.add(description.value);
-  let tokenURI = `https://ipfs.infura.io/ipfs/${cid.path}/?token=`;
+  let tokenURI = await uploadDescription(name.value, description.value);
   console.log(tokenURI);
   store.state.blockchain
     .deployNew(name.value, "EAG", tokenURI, "0.001")
